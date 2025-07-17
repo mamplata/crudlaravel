247aaeb89866734fdf8e755cb5a5ea44fe5ac668
mamplata <deejaymamplata29@gmail.com>
2025-07-17 08:04:05 +0800

    add functionality of update and delete item

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,12 @@
 <template>
    <div>
      <h1>CRUD APPLICATION</h1>
-     <form @submit.prevent="createItem">
+     <form @submit.prevent="isEditing ? updateItem() : createItem()">
         <input v-model="form.name" placeholder="Enter name" />
         <input v-model.number="form.qty" placeholder="Enter quantity" />
         <input v-model.number="form.price" placeholder="Enter price" />
-        <button type="submit">Add Item</button>
+        <button id="submitBtn" type="submit">{{ isEditing ? "Update" : "Add Item" }}</button>
+        <button v-if="isEditing" @click="cancelEdit()">Cancel</button>
      </form>
      <table>
        <thead>
@@ -13,13 +14,16 @@
           <th>Name</th>
           <th>Quantity</th>
           <th>Price</th>
+          <th>Actions</th>
          </tr>
        </thead>
-       <tbody v-for="item in items" :key="items.id">
+       <tbody v-for="item in items" :key="item.id">
          <tr>
            <td>{{item.name}}</td>
            <td>{{item.qty}}</td>
            <td>P{{item.price}}</td>
+           <td><button @click="editItem(item)">Edit</button>
+           <button @click="deleteItem(item.id)">Delete</button></td>
          </tr>
          
        </tbody>
@@ -38,6 +42,8 @@ const form = ref({
 });
 
 const items = ref([]);
+const isEditing = ref(false);
+const editId = ref(null);
 
 const fetchItems = () => {
     axios.get("/items").then(res => {
@@ -52,6 +58,35 @@ const createItem = () => {
    })
 }
 
+const editItem = (item) => {
+  form.value = {...item};
+  editId.value = item.id;
+  isEditing.value = true;
+}
+
+const cancelEdit = () => {
+  form.value = {name: "", qty: 0, price: 0.0}
+  editId.value = null;
+  isEditing.value = false;
+}
+
+const updateItem = () => {
+  axios.put(`/items/${editId.value}`, form.value).then(() => {
+     form.value = {name: "", qty: 0, price: 0.0};
+     editId.value = null;
+     isEditing.value = false;
+     fetchItems();
+  })
+}
+
+const deleteItem = (id) => {
+    if(confirm("Are you sure you want to delete this item?")) {
+        axios.delete(`/items/${id}`).then(() => {
+          fetchItems();
+        })
+    }
+}
+
 onMounted(() => {
    fetchItems();
 })
@@ -59,4 +94,29 @@ onMounted(() => {
 </script>
 
 <style scoped>
+
+table {
+  width: 100%;
+}
+
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+  margin-top: 20px;
+  padding: 10px;
+  text-align: center;
+}
+
+#submitBtn {
+  margin-left: 20px;
+}
+
+button {
+  background-color: blue;
+  color: white;
+  border: none;
+  border-radius: 10px;
+  padding: 10px;
+}
+
 </style>
